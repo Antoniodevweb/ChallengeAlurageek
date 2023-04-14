51ac5d74db9073b308aa9bae070dd1a202dc7612
Antoniodevweb <antonio.perez7922@alumnos.udg.mx>
2023-04-13 18:02:08 -0600

    validation form and text area fixed

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const stars = document.querySelectorAll(".product__stars--container i")
-console.log(stars)
-stars.forEach((star, index1) =>{
-    star.addEventListener("click" , () => {
-        stars.forEach((star, index2) =>{
-                index1 >= index2 ? star.classList.add("active") : star.classList.remove("active")
-            })
-    });
+const starsContainers = document.querySelectorAll('.product__stars--container');
+
+starsContainers.forEach((starContainer) => {
+	const stars = starContainer.querySelectorAll('li i');
+
+	stars.forEach((star, index1) => {
+		star.addEventListener('click', () => {
+			stars.forEach((star, index2) => {
+				star.style.color =
+					index1 >= index2 ? 'var(--star-active)' : 'var(--grey-50)';
+			});
+		});
+	});
 });
